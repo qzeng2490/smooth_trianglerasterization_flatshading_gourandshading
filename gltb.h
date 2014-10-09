233bf906c58fa4bbfdd926d244816fa76ceddd21
Qiang <qzeng2490@yahoo.com>
2014-10-08 22:05:19 -0400

    Create gltb.h

---
+++ gltb.h
@@ -0,0 +1,24 @@
+
+
+#ifndef __aaaaaa__gltb__
+#define __aaaaaa__gltb__
+
+void
+gltbInit(GLuint button);
+
+void
+gltbMatrix(void);
+
+void
+gltbReshape(int width, int height);
+
+void
+gltbMouse(int button, int state, int x, int y);
+
+void
+gltbMotion(int x, int y);
+
+void
+gltbAnimate(GLboolean animate);
+
+#endif /* defined(__aaaaaa__gltb__) */
